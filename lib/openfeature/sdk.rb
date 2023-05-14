b220cfa9b7f4f868554a1a1c9f5f73bd12c5e76d
Max VelDink <maxveldink@gmail.com>
2023-05-14 06:55:34 -0400

    Initial commit

---
+++ lib/openfeature/sdk.rb
@@ -0,0 +1,8 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "sorbet-runtime"
+require "zeitwerk"
+
+# Sorbet-aware namespace to super-charge your projects
+module OpenFeature; end
---
+++ sorbet/tapioca/require.rb
@@ -0,0 +1,8 @@
+# typed: true
+# frozen_string_literal: true
+
+require "debug"
+require "minitest/autorun"
+require "openfeature/sdk"
+require "sorbet-runtime"
+require "zeitwerk"
