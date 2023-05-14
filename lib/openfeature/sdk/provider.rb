4238c2626654b85b8b6e47f2169ecf2f0914a493
Max VelDink <maxveldink@gmail.com>
2023-05-14 13:24:01 -0400

    Finish provider interface

--- lib/openfeature/sdk/provider.rb
+++ lib/openfeature/sdk/provider.rb
@@ -12,6 +12,9 @@ module OpenFeature
       sig { abstract.returns(ProviderMetadata) }
       def metadata; end
 
+      sig { abstract.returns(T::Array[Hook]) }
+      def hooks; end
+
       sig do
         abstract
           .params(
@@ -22,6 +25,39 @@ module OpenFeature
           .returns(ResolutionDetails[T::Boolean])
       end
       def resolve_boolean_value(flag_key:, default_value:, context: nil); end
+
+      sig do
+        abstract
+          .params(
+            flag_key: String,
+            default_value: String,
+            context: T.nilable(EvaluationContext)
+          )
+          .returns(ResolutionDetails[String])
+      end
+      def resolve_string_value(flag_key:, default_value:, context: nil); end
+
+      sig do
+        abstract
+          .params(
+            flag_key: String,
+            default_value: Numeric,
+            context: T.nilable(EvaluationContext)
+          )
+          .returns(ResolutionDetails[Numeric])
+      end
+      def resolve_number_value(flag_key:, default_value:, context: nil); end
+
+      sig do
+        abstract
+          .params(
+            flag_key: String,
+            default_value: T::Hash[T.untyped, T.untyped],
+            context: T.nilable(EvaluationContext)
+          )
+          .returns(ResolutionDetails[T::Hash[T.untyped, T.untyped]])
+      end
+      def resolve_structure_value(flag_key:, default_value:, context: nil); end
     end
   end
 end
