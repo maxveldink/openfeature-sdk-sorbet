5397898448bd7a38f5881a18abd1eedb7d162d70
Max VelDink <maxveldink@gmail.com>
2023-05-16 12:03:50 -0400

    Add eval context fields

--- lib/open_feature/evaluation_context.rb
+++ lib/open_feature/evaluation_context.rb
@@ -1,10 +1,40 @@
 # typed: strict
 # frozen_string_literal: true
 
+require "date"
+
 module OpenFeature
   # Provides ambient information for the purposes of flag evaluation.
   # Currently does not meet specification!
   class EvaluationContext < T::Struct
+    extend T::Sig
+
+    FieldValueType = T.type_alias { T.any(T::Boolean, String, Numeric, DateTime, Structure) }
+
     const :targeting_key, T.nilable(String)
+    const :fields, T::Hash[String, FieldValueType], default: {}
+
+    sig { params(method_name: Symbol).returns(T::Boolean) }
+    def respond_to_missing?(method_name)
+      fields.key?(method_name.to_s)
+    end
+
+    sig { params(method_name: Symbol).returns(T.nilable(FieldValueType)) }
+    def method_missing(method_name)
+      fields.fetch(method_name.to_s, nil)
+    end
+
+    sig { params(key: String, value: FieldValueType).returns(EvaluationContext) }
+    def add_field(key, value)
+      EvaluationContext.new(
+        targeting_key: targeting_key,
+        fields: fields.merge({ key => value })
+      )
+    end
+
+    sig { returns(T::Hash[String, FieldValueType]) }
+    def to_h
+      targeting_key.nil? ? fields : fields.merge("targeting_key" => targeting_key)
+    end
   end
 end
---
+++ test/open_feature/evaluation_context_test.rb
@@ -0,0 +1,45 @@
+# typed: true
+# frozen_string_literal: true
+
+require "test_helper"
+
+class EvaluationContextTest < Minitest::Test
+  def setup
+    @evaluation_context_without_targeting_key = OpenFeature::EvaluationContext.new(
+      fields: { "testing" => "a value" }
+    )
+    @evaluation_context_with_targeting_key = OpenFeature::EvaluationContext.new(
+      targeting_key: "abc",
+      fields: { "testing" => "a value" }
+    )
+  end
+
+  def test_method_missing_returns_field_value_if_found
+    assert_equal("a value", @evaluation_context_without_targeting_key.testing)
+  end
+
+  def test_method_missing_returns_nil_if_not_found
+    assert_nil(@evaluation_context_without_targeting_key.not_here)
+  end
+
+  def test_add_field_returns_new_context_instance
+    new_context = @evaluation_context_without_targeting_key.add_field("another", "thing")
+
+    refute_equal(@evaluation_context_without_targeting_key, new_context)
+    assert_equal("thing", new_context.another)
+  end
+
+  def test_to_h_returns_hash_of_all_fields_without_targeting_key
+    assert_equal(
+      { "testing" => "a value" },
+      @evaluation_context_without_targeting_key.to_h
+    )
+  end
+
+  def test_to_h_returns_hash_of_all_fields_with_targeting_key
+    assert_equal(
+      { "testing" => "a value", "targeting_key" => "abc" },
+      @evaluation_context_with_targeting_key.to_h
+    )
+  end
+end
